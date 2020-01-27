c5c7227a1e78c7a5fe6286a2d8ab252895cae093
crusiris <cruzsirys@gmail.com>
2020-01-27 15:51:26 -0300

    Creando los componentes de la aplicacion

---
+++ src/app/components/shared/container-avatar/container-avatar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-container-avatar',
+  templateUrl: './container-avatar.component.html',
+  styleUrls: ['./container-avatar.component.css']
+})
+export class ContainerAvatarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
