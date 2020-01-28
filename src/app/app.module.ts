dfadbd48a83b6bec77aba3ee6b8c71acb4c0a137
crusiris <cruzsirys@gmail.com>
2020-01-28 17:44:08 -0300

    INSTALANDO  AngularFire Y CONFIGURANDO los modulos de angularFire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+/*Modulos de angularfire*/ 
+import { AngularFireModule } from '@angular/fire';
+import { environment } from '../environments/environment';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -25,7 +31,11 @@ import { StartComponent } from './components/start/start.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule, //importa firebase / firestore, solo es necesario para las características de la base de datos
+    AngularFireAuthModule, // importa firebase / auth, solo es necesario para las funciones de autenticación,
+    AngularFireStorageModule //Importa Firebase / Storage solo es necesario para las funciones de almacenamiento
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,10 +2,19 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
-  production: false
-};
 
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDzMu-XrAg0x7kS3-STlwyRZp81zYJeuf0",
+    authDomain: "naturalizate-74cf9.firebaseapp.com",
+    databaseURL: "https://naturalizate-74cf9.firebaseio.com",
+    projectId: "naturalizate-74cf9",
+    storageBucket: "naturalizate-74cf9.appspot.com",
+    messagingSenderId: "776717583088",
+    appId: "1:776717583088:web:595eb73462ccbbeee57835"
+  }
+};
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
